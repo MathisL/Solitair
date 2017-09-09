ed2cc9ff47f23e14e20427ad04472e82a4eb58dd
MathisL <smear1@gmx.de>
2017-09-09 17:57:48 +0200

    Added main

---
+++ Solitaire/__main__.py
@@ -0,0 +1,10 @@
+'''
+Created on 09.09.2017
+
+@author: Tibu
+'''
+
+import de.mathis.Solitaire as S
+
+if __name__ == '__main__':
+    S.main()
--- Solitaire/de/mathis/Solitaire.py
+++ Solitaire/de/mathis/Solitaire.py
@@ -6,28 +6,30 @@ Created on 09.09.2017
 '''
 from de.mathis.SolitaireBoard import SolitaireBoard 
 
-print('Hi! Welcome to this simple solitair game. Have fun and good luck! (press h for help, x to quit)')
 
-board = SolitaireBoard()
-board.printBoardAndIndices()
+def main():
+    print('Hi! Welcome to this simple solitair game. Have fun and good luck! (press h for help, x to quit)')
+
+    board = SolitaireBoard()
+    board.printBoardAndIndices()
 
 
-def printHelp():
-    print('sorry I have no idea how this works either')
+    def printHelp():
+        print('sorry I have no idea how this works either')
 
-while(True):
-    print('Enter your move (stone and target separated by space):')
-    read = input()
-    if read.startswith('x'):
-        print('Bye now!')
-        break
-    if read.startswith('h'):
-        printHelp()
-        continue
-    split = read.split(sep=' ')
-    try:
-        a = int(split[0])
-        b = int(split[1])
-        board.moveAndPrint(a, b)
-    except: 
-        print('Invalid input')
+    while(True):
+        print('Enter your move (stone and target separated by space):')
+        read = input()
+        if read.startswith('x'):
+            print('Bye now!')
+            break
+        if read.startswith('h'):
+            printHelp()
+            continue
+        split = read.split(sep=' ')
+        try:
+            a = int(split[0])
+            b = int(split[1])
+            board.moveAndPrint(a, b)
+        except: 
+            print('Invalid input')
